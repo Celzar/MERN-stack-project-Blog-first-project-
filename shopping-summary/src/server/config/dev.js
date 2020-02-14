7c7a8dafc123a0c733d7766173546351d5d12c7e
Celza <xaysanaphoneZ@outlook.com>
2020-02-14 18:24:49 +0700

    forget

--- shopping-summary/src/server/config/dev.js
+++ shopping-summary/src/server/config/dev.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb+srv://Hello:<password>@cluster-heroku-jdzkb.mongodb.net/test?retryWrites=true&w=majority'
+  mongoURI: 'mongodb+srv://Hello:abc1234@cluster-heroku-jdzkb.mongodb.net/test?retryWrites=true&w=majority'
+  
 }
